eea1d0518466612e19727741e5c3b61105021cf1
DESKTOP-N6PNFCS\hajna <hajnal.balint.david@gmail.com>
2020-09-08 21:50:09 +0200

    automatically using latest file, HR from RR (no logfile, only IBI), minor comments and restructure

--- Base.gd
+++ Base.gd
@@ -2,56 +2,74 @@ extends Control
 
 onready var currentHR = 0
 onready var currentRR = 0
+onready var RR_array = []
 onready var usedRR_arr = []
 
-export var RR_use_amount = 5 # TODO only for RR currently, add to HR
+onready var latest_file
+
+export (int, 9999) var RR_use_amount = 5
+
+export (String) var folder_location = "C:/Users/hajna/HeartRateLogs"
+
+
+func _ready():
+	initFile(folder_location)
 
 func _on_Timer_timeout():
-	updateHR() 	# from logfile 
-				# can also calculate custom HR from RR
-	updateRR() 	# from IBI file
+	updateRR() 	# from IBI file (logfile unused)
 	
 	# https://en.wikipedia.org/wiki/Heart_rate_variability#Analysis
 	# https://imotions.com/blog/heart-rate-variability/
 	
-	RMSSD() # TODO detect trend? separate timer, run less often?
+	# TIME domain
+	HR()
+	RMSSD() 
 	# SDNN() # add maybe?
+	# Baevsky’s stress index
 	
-	# frequency domain measurements -> measure of sympathetic nervous system activity
+	# FREQUENCY domain measurements -> measure of sympathetic nervous system activity
 	# frequencies [Hz]:  # TODO export?
 		# HF: 0.15 - 0.4
 		# LF: 0.04 - 0.15
-		# VLF: 0.0033 - 0.04
+		# VLF: 0 - 0.04
 
-func updateHR():
-	
-	var csv_array = []
-	var csv_file = File.new();
-	
-	csv_file.open("C:/Users/hajna/HeartRateLogs/heartrateCSV.csv", csv_file.READ)
-	
-	while not csv_file.eof_reached():
-		var csv_row = []
-		var csv_line = csv_file.get_line()
-		if csv_line != "":	# needed for last line (\n in HeartRate program)
-			for element in csv_line.split(","):
-				csv_row.append(element);
-			csv_array.append(csv_row);
+	# TODO detect trends, graph, separate timer, run less often?
 
-	var csv_size_column = csv_array.size();		# number of rows
-	var _csv_size_row = csv_array[0].size();	# number of columns
-	var csv_lastRow = csv_size_column-1;
-	
-	currentHR = str(csv_array[csv_lastRow][2])	
-	$HR_label.text = currentHR
-	
+	pass
+
+func _on_Button_button_down():
+	RMSSD()
+
+func initFile(path):
+	var latest_date = 0
+	var date_modified
+	var dir = Directory.new()
+	if dir.open(path) == OK:
+		dir.list_dir_begin()
+		var file_name = dir.get_next()
+		while file_name != "":
+			if !dir.current_is_dir() && file_name.get_extension() == "txt": # IBI only?
+				print("Found file: " + file_name)
+				print("File extension: " + file_name.get_extension())
+
+				var file = File.new()
+				date_modified = file.get_modified_time(path.plus_file(file_name))
+				print("File last modified: " + str(date_modified) + "\n")
+				
+				if date_modified > latest_date:
+					latest_date = date_modified
+					latest_file = file_name
+
+			file_name = dir.get_next()
+		print("Latest file: " + latest_file)
+	else:
+		print("An error occurred when trying to access the path.")
+
 func updateRR():
-	var RR_array = []
-	var IBI_file = File.new()
 
 	# TODO get_modified_time -> use the latest reading, timestamp
-	# TODO put file open outside updateRR()  if possible?
-	IBI_file.open("C:/Users/hajna/HeartRateLogs/IBI.txt", IBI_file.READ)
+	var IBI_file = File.new()
+	IBI_file.open(folder_location.plus_file(latest_file), IBI_file.READ)
 	
 	# TODO save byte position of last read, only read data after that
 	while not IBI_file.eof_reached():
@@ -59,7 +77,7 @@ func updateRR():
 		if IBI_line != "":	# needed for last line (\n in HeartRate program)
 			RR_array.append(IBI_line)
 	
-	# TODO szures?
+	# TODO data filtering?
 	
 	var IBI_size = RR_array.size()
 	var IBI_lastRow = IBI_size-1 # last IBI value in file
@@ -70,6 +88,14 @@ func updateRR():
 	currentRR = str(RR_array[IBI_lastRow])
 	$RR_label.text = currentRR
 
+func HR():
+	currentHR = 0
+	for RR in usedRR_arr:
+		currentHR += int(RR)
+	currentHR = currentHR / RR_use_amount * 60 / 1000
+	
+	$HR_label.text = str(currentHR)
+
 func RMSSD():
 	var RMSSD = 0
 	
@@ -79,5 +105,3 @@ func RMSSD():
 	RMSSD = sqrt(RMSSD)
 	$RMSSD_label.text = str(RMSSD)
 
-func _on_Button_button_down():
-	RMSSD()
