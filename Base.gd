1be1d0bae4759b6d74f5eef81a58773b3573c78f
BALINT-LAPTOP\hajna <hajnal.balint.david@gmail.com>
2020-09-14 23:07:12 +0200

    added results array (wip), EasyCharts for charting data

--- Base.gd
+++ Base.gd
@@ -1,17 +1,17 @@
 extends Control
 
-onready var HR = 0
 onready var currentRR = 0
 onready var RR_average = 0
-
+onready var HR = 0
 onready var RMSSD = 0
 onready var SDNN = 0
 onready var pNN50: float = 0
 onready var pNN20: float = 0
 onready var SI = 0
 
-onready var RR_array = []
-onready var usedRR_arr = []
+onready var RR_arr = []
+onready var RR_used_arr = []
+onready var results_arr = []
 
 onready var latest_file
 onready var filePos = 0
@@ -21,6 +21,10 @@ export (String) var folder_location = "C:/Users/hajna/HeartRateLogs"
 
 func _ready():
 	initFile(folder_location)
+	drawCharts()
+	
+	var titleRow = ["Data_number","HR","RMMSD","SDNN","pNN50","pNN20","SI"]
+	results_arr.append(titleRow)
 
 func _on_Timer_timeout():
 	updateRR() 	# from IBI file (logfile unused)
@@ -53,7 +57,9 @@ func _on_Timer_timeout():
 
 	# TODO detect trends, graph, separate timer, run less often?
 
-	pass
+	# writing results to array and CSV (for plotting)
+	results_arr.append([1,HR,RMSSD,SDNN,pNN50,pNN20,SI])
+	
 
 func _on_Button_button_down():
 	pass
@@ -92,37 +98,37 @@ func updateRR():
 	while not IBI_file.eof_reached():
 		var IBI_line = IBI_file.get_line()
 		if IBI_line != "":	# needed for last line (\n in HeartRate program)
-			RR_array.append(int(IBI_line))
+			RR_arr.append(int(IBI_line))
 
 	filePos = IBI_file.get_position()
 
-	var IBI_size = RR_array.size()
+	var IBI_size = RR_arr.size()
 
-	usedRR_arr = RR_array.slice(IBI_size - RR_use_amount, IBI_size) # takes last "RR_use_amount" values of IBI file
+	RR_used_arr = RR_arr.slice(IBI_size - RR_use_amount, IBI_size) # takes last "RR_use_amount" values of IBI file
 
-	print_debug(usedRR_arr)
+	print_debug(RR_used_arr)
 
 	RR_average = 0
 	for i in range(RR_use_amount):
-		RR_average += usedRR_arr[i]
+		RR_average += RR_used_arr[i]
 	RR_average = RR_average / RR_use_amount
 
-	currentRR = str(RR_array[IBI_size - 1])
+	currentRR = str(RR_arr[IBI_size - 1])
 	IBI_file.close()
 
 func HR_func():
 	var HR_calc = 0.0
-	for RR in usedRR_arr:
+	for RR in RR_used_arr:
 		HR_calc += RR
 	HR_calc = 60 / (HR_calc / RR_use_amount) * 1000
-		
+	
 	return HR_calc
 
 func RMSSD_func():
 	var RMSSD_calc = 0
 	
 	for i in range(RR_use_amount-1):	# -1 because N elements, but N-1 places between elements
-		RMSSD_calc += pow(usedRR_arr[i] - usedRR_arr[i + 1], 2)
+		RMSSD_calc += pow(RR_used_arr[i] - RR_used_arr[i + 1], 2)
 	
 	RMSSD_calc = sqrt(RMSSD_calc / (RR_use_amount-1))
 	
@@ -132,7 +138,7 @@ func pNNX_func(x):
 	var NNX: int = 0
 
 	for i in range(RR_use_amount-1):
-		if abs(usedRR_arr[i] - usedRR_arr[i + 1]) > x: 
+		if abs(RR_used_arr[i] - RR_used_arr[i + 1]) > x: 
 			NNX += 1
 	var pNNX = float(NNX) / float(RR_use_amount-1) * 100
 	
@@ -142,24 +148,23 @@ func SDNN_func():
 	var SDNN_calc = 0
 	
 	for i in range(RR_use_amount):
-		SDNN_calc += pow(usedRR_arr[i] - RR_average,2)
+		SDNN_calc += pow(RR_used_arr[i] - RR_average,2)
 	SDNN_calc = sqrt(SDNN_calc / RR_use_amount)
 
 	return SDNN_calc
 	
 func SI_func():
-	var SI_calc = 0 # Stress Index
-	var AMo = 0		# amplitude of the modal value
 	var Mo = 0 		# RR median -> should be mode in original paper?
-	var MxDMn = (float(usedRR_arr.max()) - float(usedRR_arr.min())) # variability width
+	var AMo = 0		# amplitude of the modal value
+	var MxDMn = (float(RR_used_arr.max()) - float(RR_used_arr.min())) # variability width
 
-	var RR_array_sorted = usedRR_arr
-	RR_array_sorted.sort()
+	var RR_arr_sorted = RR_used_arr
+	RR_arr_sorted.sort()
 
 	if RR_use_amount % 2 != 0: 			# odd RR_use_amount
-		Mo = float(RR_array_sorted[RR_use_amount/2])
+		Mo = float(RR_arr_sorted[RR_use_amount/2])
 	else:								# even RR_use_amount -> mean
-		Mo = (float(RR_array_sorted[RR_use_amount/2]) + float(RR_array_sorted[(RR_use_amount/2)-1])) / 2
+		Mo = (float(RR_arr_sorted[RR_use_amount/2]) + float(RR_arr_sorted[(RR_use_amount/2)-1])) / 2
 
 	var SI_array = []
 	var SI_array_index = -1
@@ -167,7 +172,7 @@ func SI_func():
 	var prevValue = 0
 
 	for i in range (RR_use_amount):
-		steppedValue = stepify(RR_array_sorted[i], 50) # ...or using format strings
+		steppedValue = stepify(RR_arr_sorted[i], 50) # ...or using format strings
 
 		if steppedValue != prevValue: 	# start new element in array
 			SI_array.append(int(0))
@@ -178,7 +183,7 @@ func SI_func():
 
 	AMo = float(100 * SI_array.max()) / float(RR_use_amount)
 
-	SI_calc = sqrt(float(AMo) / float(2 * Mo) * MxDMn)
+	var SI_calc = sqrt(float(AMo) / float(2 * Mo) * MxDMn)
 # Stress index (SI) = AMo / 2Mo x MxDMn. Where AMo is the amplitude of the modal
 # value and represents the percentage in comparison to all other RR intervals. 
 # Mo (in the formula 2Mo) is the modal value for the duration of a RR interval 
@@ -186,3 +191,6 @@ func SI_func():
 # other words the difference between the maximum and minimum measured RR interval. 
 
 	return SI_calc
+
+func drawCharts():
+	$LineChart.plot()
